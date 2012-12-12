web-animations/web-animations-js-legacy

commit 2998dbbd9c35c748cba75ee9990a621ae60f2fa9

Plumb operation through AnimFunc and provide test.

web-animation.js
@@ -146,7 +146,7 @@ var TimedTemplate = Class.create({
 });
 
 function exists(val) {
-  return typeof val !== 'undefined';
+  return typeof val !== 'undefined' && (val !== null);
 }
 
 var ST_MANUAL = 0;
@@ -1041,7 +1041,7 @@ AnimFunc.createFromProperties = function(properties) {
     return null;
   } else if (animProps.length === 1) {
     return AnimFunc._createKeyframeFunc(
-        animProps[0], properties[animProps[0]]);
+        animProps[0], properties[animProps[0]], properties.operation);
   } else {
     // TODO: GroupAnimFunc
     try {
@@ -1051,7 +1051,7 @@ AnimFunc.createFromProperties = function(properties) {
 }
 
 // Step 3 - Create a KeyframeAnimFunc object
-AnimFunc._createKeyframeFunc = function(property, value) {
+AnimFunc._createKeyframeFunc = function(property, value, operation) {
   var func = new KeyframeAnimFunc(property);
 
   if (typeof value === 'string') {
@@ -1074,6 +1074,10 @@ AnimFunc._createKeyframeFunc = function(property, value) {
     } catch (e) { console.log(e.stack); throw e; }
   }
   // TODO: Need to handle KeyframeDict objects once they're defined
+
+  if (exists(operation)) {
+    func.operation = operation;
+  }
 
   return func;
 }
@@ -1265,16 +1269,18 @@ var TimingFunc = Class.create({
   }
 });
 
-function _interp(from, to, f) {
+function _interp(from, to, f, type) {
   if (Array.isArray(from) || Array.isArray(to)) {
-    return _interpArray(from, to, f);
+    return _interpArray(from, to, f, type);
   }
-  to   = to   || 0.0;
-  from = from || 0.0;
+  var zero = type == "scale" ? 1.0 : 0.0;
+  to   = exists(to) ? to : zero;
+  from = exists(from) ? from : zero;
+
   return to * f + from * (1 - f);
 }
 
-function _interpArray(from, to, f) {
+function _interpArray(from, to, f, type) {
   console.assert(Array.isArray(from) || from === null,
     'From is not an array or null');
   console.assert(Array.isArray(to) || to === null,
@@ -1285,7 +1291,7 @@ function _interpArray(from, to, f) {
 
   var result = [];
   for (var i = 0; i < length; i++) {
-    result[i] = _interp(from ? from[i] : 0.0, to ? to[i] : 0.0, f);
+    result[i] = _interp(from ? from[i] : null, to ? to[i] : null, f, type);
   }
   return result;
 }
@@ -1385,7 +1391,7 @@ function interpolate(property, target, from, to, f) {
         to[i].t === null,
         'Transform types should match or one should be the underlying value');
       var type = from[i].t ? from[i].t : to[i].t;
-      out.push({t: type, d:_interp(from[i].d, to[i].d, f)});
+      out.push({t: type, d:_interp(from[i].d, to[i].d, f, type)});
     }
     return toCssValue(property, out, svgMode);
   } else {
@@ -1470,7 +1476,7 @@ function extractTranslateValue(length) {
 
 function extractScaleValues(scales) {
   var scaleX = Number(scales[1]);
-  var scaleY = scales[2] ? Number(lengths[2]) : scaleX;
+  var scaleY = scales[2] ? Number(scales[2]) : scaleX;
   return [scaleX, scaleY];
 }
 
@@ -1713,12 +1719,12 @@ DEFAULT_GROUP.currentState = function() {
 
 // If requestAnimationFrame is unprefixed then it uses high-res time.
 function initTiming() {
-  useHighResTime = 'requestAnimationFrame' in window;
-  window.requestAnimationFrame = window.requestAnimationFrame ||
-    window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame ||
-    window.msRequestAnimationFrame; // 80 wrap is so 80s
-  timeNow = undefined;
-  timeZero = useHighResTime ? 0 : Date.now();
+	useHighResTime = 'requestAnimationFrame' in window;
+	window.requestAnimationFrame = window.requestAnimationFrame ||
+		window.webkitRequestAnimationFrame || // 80 wrap is so 80s
+		window.mozRequestAnimationFrame || window.msRequestAnimationFrame;
+	timeNow = undefined;
+	timeZero = useHighResTime ? 0 : Date.now();
 }
 
 initTiming();

test-compositor.html
@@ -1,0 +1,37 @@
+<!-- 
+Copyright 2012 Google Inc. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+
+<!DOCTYPE html>
+<svg>
+	<rect class="anim" x="100px" y="10px" width="100px" height="100px"></rect>
+	<rect class="anim2" x="100px" y="10px" width="100px" height="100px"></rect>
+</svg>
+<script src="http://ajax.googleapis.com/ajax/libs/prototype/1.7.1.0/prototype.js"></script>
+<script src="web-animation.js"></script>
+<script>
+var rect = document.querySelector(".anim")
+var rect2 = document.querySelector(".anim2")
+
+new Anim(rect, {x: ["0px", "400px"]}, 3);
+new Anim(rect, {x: ["0px", "200px"], operation: 'add'}, {startDelay: 1, duration: 4});
+new Anim(rect, {x: ["0px", "200px"], operation: 'merge'}, {startDelay: 2, duration: 5});
+
+new Anim(rect2, {"-webkit-transform": ["translate(0px, 0px)", "translate(200px, 200px)"]}, 3);
+new Anim(rect2, {"-webkit-transform": ["rotate(0deg) translate(0px, 0px)", 
+                                       "rotate(90deg) translate(100px, 0px)"], operation: 'add'}, {startDelay: 1, duration: 4});
+new Anim(rect2, {"-webkit-transform": ["translate(0px, 0px) rotate(0deg) translate(0px, 0px) scale(1, 1)", 
+                                       "translate(0px, 0px) rotate(0deg) translate(0px, 0px) scale(2, 2)"], operation: 'merge'}, {startDelay: 2, duration: 5});
+</script>

shans/web-anim-js

commit f520269ecbe601bac4826cde988261b9f522cdd5

Fix use of isDefinedAndNotNull()
When processing arguments to API functions, we generally want to test only
whether a variable is defined. We then rely on the regular JavaScript type
conversion rules for handling values of the wrong type, including null.
Introduce a new isDefined() helper for this case.

Also fix the test for a parent in TimedItem(). This was broken in
2998dbbd9c35c748cba75ee9990a621ae60f2fa9, which caused test-parent to fail.

web-animation.js
@@ -48,7 +48,7 @@ var Timing = function(timingDict) {
   if (this.duration < 0.0) {
     throw new IndexSizeError('duration must be >= 0');
   }
-  this.iterationCount = isDefinedAndNotNull(timingDict.iterationCount) ?
+  this.iterationCount = isDefined(timingDict.iterationCount) ?
       timingDict.iterationCount : 1.0;
   if (this.iterationCount < 0.0) {
     throw new IndexSizeError('iterationCount must be >= 0');
@@ -57,7 +57,7 @@ var Timing = function(timingDict) {
   if (this.iterationStart < 0.0) {
     throw new IndexSizeError('iterationStart must be >= 0');
   }
-  this.playbackRate = isDefinedAndNotNull(timingDict.playbackRate) ?
+  this.playbackRate = isDefined(timingDict.playbackRate) ?
       timingDict.playbackRate : 1.0;
   //this.playbackRate = timingDict.playbackRate || 1.0;
   this.direction = timingDict.direction || 'normal';
@@ -179,8 +179,12 @@ mixin(TimedTemplate.prototype, {
   }
 });
 
+var isDefined = function(val) {
+  return typeof val !== 'undefined';
+};
+
 var isDefinedAndNotNull = function(val) {
-  return typeof val !== 'undefined' && (val !== null);
+  return isDefined(val) && (val !== null);
 };
 
 var ST_MANUAL = 0;
@@ -199,7 +203,7 @@ var TimedItem = function(timing, startTime, parentGroup) {
   this.animationTime = null;
   this._reversing = false;
 
-  if (!isDefinedAndNotNull(parentGroup)) {
+  if (!isDefined(parentGroup)) {
     this.parentGroup = DEFAULT_GROUP;
   } else if (parentGroup === null || parentGroup instanceof TimedItem) {
     this.parentGroup = parentGroup;
@@ -207,7 +211,7 @@ var TimedItem = function(timing, startTime, parentGroup) {
     throw new TypeError('parentGroup is not a TimedItem');
   }
 
-  if (!isDefinedAndNotNull(startTime)) {
+  if (!isDefined(startTime)) {
     this._startTimeMode = ST_AUTO;
     if (this.parentGroup) {
       this._startTime = this.parentGroup.iterationTime || 0;
@@ -308,7 +312,7 @@ mixin(TimedItem.prototype, {
   },
   // TODO: take timing.iterationStart into account. Spec needs to as well.
   updateIterationDuration: function() {
-    if (isDefinedAndNotNull(this.timing.duration)) {
+    if (isDefined(this.timing.duration)) {
       this.duration = this.timing.duration;
     } else {
       this.duration = this.intrinsicDuration();
@@ -332,7 +336,7 @@ mixin(TimedItem.prototype, {
     if (this.parentGroup && this.parentGroup.iterationTime) {
       this.itemTime = this.parentGroup.iterationTime -
           this._startTime - this.timeDrift;
-    } else if (isDefinedAndNotNull(parentTime)) {
+    } else if (isDefined(parentTime)) {
       this.itemTime = parentTime;
     } else {
       this.itemTime = null;
@@ -534,7 +538,7 @@ var interpretAnimFunc = function(animFunc) {
 };
 
 var interpretTimingParam = function(timing) {
-  if (!isDefinedAndNotNull(timing) || timing === null) {
+  if (!isDefinedAndNotNull(timing)) {
     return new Timing({});
   }
   if (timing instanceof Timing || timing instanceof TimingProxy) {
@@ -822,7 +826,7 @@ var AnimListMixin = {
     return removedItems;
   },
   remove: function(index, count) {
-    if (!isDefinedAndNotNull(count)) {
+    if (!isDefined(count)) {
       count = 1;
     }
     return this.splice(index, count);